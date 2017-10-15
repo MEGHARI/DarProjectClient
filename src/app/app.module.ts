8570fe4812e90ffdaf30d7ace38070f1c96c918f
MEGHARI <meghari.aghiles@yahoo.fr>
2017-10-15 13:17:57 +0200

    fix errors

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import {NgForm} from "@angular/forms"
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import  {AboutComponent} from "./about/about.component";
+import { AppComponent } from './home/app.component';
+import { FormComponent } from "./form/form.component";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+   { path: 'about', component: AboutComponent },
+   { path: 'form', component: FormComponent }
+];
+
+@NgModule({
+  imports: [BrowserModule,HttpModule,RouterModule.forRoot(appRoutes),FormsModule],
+  declarations: [AppComponent,FormComponent,AboutComponent],
+  bootstrap: [AppComponent]
+
+  
+})
+export class AppModule {}
+
