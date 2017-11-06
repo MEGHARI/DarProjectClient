b69f6d8f7d46365a8c52c6d79e68de8c6bc234f2
MEGHARI <meghari.aghiles@yahoo.fr>
2017-11-06 13:40:46 +0100

    correction

--- src/app/admin/navbar/navbar.component.ts
+++ src/app/admin/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'navbar-name',
+    selector: 'navbar-admin',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css']
 })
