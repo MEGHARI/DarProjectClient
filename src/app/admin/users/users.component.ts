de5e68ee80888f98e3ffc2fb508f8c6e9ac7fb49
MEGHARI <meghari.aghiles@yahoo.fr>
2017-11-07 23:35:45 +0100

    correction

---
+++ src/app/admin/users/users.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+@Component({
+    selector: 'users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css']
+})
+export class UsersAdminComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { 
+        $(document).ready(function() {
+            var panels = $('.user-infos');
+            var panelsButton = $('.dropdown-user');
+            panels.hide();
+        
+            //Click dropdown
+            panelsButton.click(function() {
+                //get data-for attribute
+                var dataFor = $(this).attr('data-for');
+                var idFor = $(dataFor);
+        
+                //current button
+                var currentButton = $(this);
+                idFor.slideToggle(400, function() {
+                    //Completed slidetoggle
+                    if(idFor.is(':visible'))
+                    {
+                        currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
+                    }
+                    else
+                    {
+                        currentButton.html('<i class="glyphicon glyphicon-chevron-down text-muted"></i>');
+                    }
+                })
+            });
+        
+        
+            $('[data-toggle="tooltip"]').tooltip();
+        
+            $('button').click(function(e) {
+                e.preventDefault();
+                alert("This is a demo.\n :-)");
+            });
+        });
+    }
+}
