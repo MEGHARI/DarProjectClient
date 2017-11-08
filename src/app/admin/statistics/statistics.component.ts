4f646e5924499427e8e5432c31d54af866d4a484
MEGHARI <meghari.aghiles@yahoo.fr>
2017-11-08 14:28:28 +0100

    correction

---
+++ src/app/admin/statistics/statistics.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'statistics',
+    templateUrl: './statistics.component.html',
+    styleUrls: ['./statistics.component.css']
+})
+export class StaticticsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
